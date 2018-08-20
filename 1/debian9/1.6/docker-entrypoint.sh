5cffb0565a3d1cf6b9a395239b2d8f08d5fdc6cb
jprzychodzen <jprzychodzen@google.com>
2018-08-20 15:45:57 +0200

    Add 1.6 branch (#2)

--- 1/debian9/1.6/docker-entrypoint.sh
+++ 1/debian9/1.6/docker-entrypoint.sh
@@ -1,46 +1,33 @@
-# Copyright (c) 2018 Google LLC.
+#!/bin/bash
+# Copyright 2018 Google Inc.
 #
 # The MIT License (MIT)
-#  
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
 # the Software without restriction, including without limitation the rights to
 # use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
 # the Software, and to permit persons to whom the Software is furnished to do so,
 # subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
 # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# 
 
-setup:
-# Run InfluxDB instance and configure admin accout with the following credentials: login=root, password=root
-- command: [docker, run, --name, some-influxdb-$UNIQUE-id, -e INFLUXDB_HTTP_AUTH_ENABLED=true -e INFLUXDB_ADMIN_USER=root -e INFLUXDB_ADMIN_PASSWORD=root, -d, $IMAGE, influxd]
-- command: [sleep, 30s]
+set -e
 
-teardown:
-- command: [docker, stop, some-influxdb-$UNIQUE-id]
-- command: [docker, rm, some-influxdb-$UNIQUE-id]
+if [ "${1:0:1}" = '-' ]; then
+    set -- influxd "$@"
+fi
 
-target: some-influxdb-$UNIQUE-id
-tests:
-- name: SHOW_DATABASES
-  # This test tries to connect to a running InfluxDB instance using admin account (login=root, password=root)
-  # end lists all databases created within a default InfluxDB
-  # if everything is correct _internal database should be displayed by this command
-  command:
-    - sh
-    - '-c'
-    - 'influx -username root -password root -execute "show databases"'
-  expect:
-    stdout:
-      matches: _internal
-    stderr:
-      mustBeEmpty: true
+if [ "$1" = 'influxd' ]; then
+    /init-influxdb.sh "${@:2}"
+fi
+
+exec "$@"
